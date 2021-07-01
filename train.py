2ddb1d4dfb3bc76e789c77b61aef7ceca3c70b95
root <michaelyeah7@126.com>
2021-07-01 04:34:29 +0000

    joint limits works

--- train.py
+++ train.py
@@ -220,7 +220,7 @@ def train():
 
             # select action with policy
             action = ppo_agent.select_action(state)
-            action.clip 
+            # action.clip 
             state, reward, done, _ = env.step(action)
             # print("action",action)
             # print("state",state)
